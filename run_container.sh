b72ee7fc315531759f0f07c4a36d8651337b3089
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-07-20 15:01:40 -0700

    all: use only ubuntu 23.04

--- run_container.sh
+++ run_container.sh
@@ -15,11 +15,7 @@ script_dir=$(readlink -f $(dirname $0))
 
 [ -d $script_dir/$image ] || die "$image image can't be found in $script_dir"
 
-# always build bases first
-podman build $script_dir/base -t linux-emulation-base:22.04 \
-    --build-arg=CLANG_VERSION=17 \
-    --build-arg=UBUNTU_VERSION=22.04 \
-    --build-arg=UBUNTU_NAME=jammy
+# always build base first
 podman build $script_dir/base -t linux-emulation-base:23.04 \
     --build-arg=CLANG_VERSION=17 \
     --build-arg=UBUNTU_VERSION=23.04 \
@@ -27,7 +23,7 @@ podman build $script_dir/base -t linux-emulation-base:23.04 \
 
 target=linux-emulation-$image
 if [ $image == base ]; then
-    target=linux-emulation-base:22.04
+    target=linux-emulation-base:23.04
 else
     podman build $script_dir/$image -t $target
 fi
